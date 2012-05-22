2cd793b49af3913270e28f0e1e2d67f6ace8b12b
Anuj Jamwal <anujjamwal@thoughtworks.com>
2012-05-22 23:47:12 +0530

    added the bare minimum mongoose functionality

---
+++ mongoose.js
@@ -0,0 +1,42 @@
+/*
+* Author : Anuj Jamwal
+* Date : May 11, 2012
+* file : mongoose.js
+*
+*/
+ var migrations = new Array();
+
+function isJsFile(file) {
+	return ((!file.isDirectory) && (file.name.lastIndexOf(".js") == (file.name.length-3)));
+}
+
+function notAlreadyExecuted(migration) {
+	return (db.Mongoose.find({"_id":migration}).count() <= 0);
+};
+
+print("Reading Migrations ...");
+listFiles("migrations").forEach(function(file) { 
+	if(isJsFile(file)) { 
+		migrations[parseInt(file.name.substr("migrations".length+1, file.name.indexOf("_")+1))] = file.name;
+	}
+});
+print((migrations.length-1)+ " Migration scripts found ...");
+
+print("Executing Migrations ...");
+migrations.forEach(function(file) {
+		if(notAlreadyExecuted(file)) {
+			try {
+				load(file);
+			} catch (error) {
+				print("error occured executing..." + file);
+				print("Aborting Mongoose....");
+				quit();
+			}
+			print(file+" : Successfully executed ..." );
+			db.Mongoose.save({"_id":file, "runTime":new Date()});
+			print("Marked Executed.....");
+		} else {
+			print(file+" is Already Executed. Skipping ... ");
+		}
+});
+
